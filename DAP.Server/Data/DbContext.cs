7702026e6c54b6218a1003bc6b214d4b10ac8634
abrahamovicR <richard.abraham.023@pslib.cz>
2025-12-10 20:03:32 +0100

    update

---
+++ DAP.Server/Data/DbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection.Emit;
+using DAP.Server.Models;
+
+namespace DAP.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        public DbSet<Items> Minigames { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=UsAgainstTheOdds.sqlite");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Minigame>(ent =>
+            {
+            ent.HasData(
+                new Minigame
+
+        ,        {
+                MinigameId = "test",
+                        Name = "Test minigame",
+                        Description = "This is a test minigame",
+
+                );
+        };
+        }
+}
---
+++ DAP.Server/Models/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace DAP.Server.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Room { get; set; }
+
+    }
+}
